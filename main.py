9a9416c428605dfea560388ad8f819b5e1f466b8
Wrek34 <rakw34@gmail.com>
2025-10-23 19:46:20 -0400

    commenting in main.py for understanding

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import sys
 from stats import get_word_count, get_char_count, sort_char_dict
 
+#error for not enough arguments
 if not len(sys.argv) == 2:
     print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
 
+#unpack and save arguments from cli
 file_name, book_path = sys.argv
 
+#read in the text from the book file
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
 def main():
-    #book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     book_wc = get_word_count(book_text)
     book_char_count = get_char_count(book_text)
     book_char_sort = sort_char_dict(book_char_count)
     print_report(book_path, book_wc, book_char_sort)
 
-
+#pretty looking info output
 def print_report(book_path, book_wc, book_char_sort):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
